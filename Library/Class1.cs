54157b74ed8fd9dc90225b73cd0e33cdc1b5abbb
KonH <konh@yandex.ru>
2019-05-23 17:41:26 +0700

    Initial commit

---
+++ Library/Class1.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Library {
+	public class Class1 { }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+using System;
+using Xunit;
+
+namespace Tests {
+	public class UnitTest1 {
+		[Fact]
+		public void Test1() { }
+	}
+}
